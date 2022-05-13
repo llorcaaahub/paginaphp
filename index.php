a08135760f8b854508018a6502c8ea10f798da29
llorcaaahub <98091174+llorcaaahub@users.noreply.github.com>
2022-05-13 09:26:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	
+	include_once("./lib/devicedetector.php");
+    if(!isMobileDevice()){
+        //Detecta que estas accedint desde un PC
+            header("Location:./PC_FILES/PC_index.php");
+        }
+    // Si es mobil te lleva al home mobil o al index mobil si no estas logueado
+    else if(isMobileDevice()){
+		header("Location:./PH_FILES/PH_index.php");
+	}
+	    	
+?>
